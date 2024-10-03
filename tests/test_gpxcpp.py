77ec872f2a1c05dd5e95db4ebb59ab8da3cbe839
Thomas Thomassen <thomas@thomthom.net>
2024-10-03 22:11:56 +0200

    Refactor tests to use fixtures.

--- tests/test_gpxcpp.py
+++ tests/test_gpxcpp.py
@@ -4,30 +4,42 @@ import gpxpy
 import pytest
 
 
-def get_gpx_length(gpx_path):
+@pytest.fixture
+def gpx_path():
+    return "gpx/2024 TopCamp/Connected_20240518_094959_.gpx"
+
+
+@pytest.fixture
+def gpx(gpx_path):
     with open(gpx_path, 'r', encoding='utf-8') as gpx_file:
         gpx = gpxpy.parse(gpx_file)
-    return gpx.length_3d()
+    return gpx
 
 
-def test_example():
-    assert 1 + 1 == 2
-
-
-def test_hello():
+def test_process_string():
     value = gpxcpp.process_string("Hello, World!")
     assert value == "Processed: Hello, World!"
 
 
-def test_tinyxml_gpx_length():
-    gpx_path = "gpx/2024 TopCamp/Connected_20240518_094959_.gpx"
+def test_tinyxml_gpx_length3d(gpx, gpx_path):
     distance = gpxcpp.tinyxml_gpx_length(gpx_path)
-    expected = get_gpx_length(gpx_path)
+    expected = gpx.length_3d()
     assert distance == pytest.approx(expected)
 
 
-def test_pugixml_gpx_length():
-    gpx_path = "gpx/2024 TopCamp/Connected_20240518_094959_.gpx"
+def test_pugixml_gpx_length3d(gpx, gpx_path):
     distance = gpxcpp.pugixml_gpx_length(gpx_path)
-    expected = get_gpx_length(gpx_path)
+    expected = gpx.length_3d()
+    assert distance == pytest.approx(expected)
+
+
+def test_pugixml_track_length3d(gpx, gpx_path):
+    expected = gpx.tracks[0].length_3d()
+    distance = gpxcpp.pugixml_gpx_length(gpx_path)
+    assert distance == pytest.approx(expected)
+
+
+def test_pugixml_segment_length3d(gpx, gpx_path):
+    expected = gpx.tracks[0].segments[0].length_3d()
+    distance = gpxcpp.pugixml_gpx_length(gpx_path)
     assert distance == pytest.approx(expected)
