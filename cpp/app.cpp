77330445c712b1da49dd0f9e134ea10aec49add2
Thomas Thomassen <thomas@thomthom.net>
2024-10-07 00:40:50 +0200

    Add fastgpx debug app.

---
+++ cpp/app.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <chrono>
+#include <filesystem>
+#include <format>
+#include <numbers>
+#include <print>
+
+#include "fastgpx.hpp"
+
+int main()
+{
+  std::println("fastgpx");
+
+  const auto relative_path = std::filesystem::path("../../../gpx/test");
+  auto path = std::filesystem::absolute(relative_path);
+  path.append("debug-segment.gpx");
+  std::println("{}", path.string());
+
+  if (!std::filesystem::exists(path))
+  {
+    std::println("File not found.");
+    return 0;
+  }
+
+  std::println("");
+  const auto gpx = fastgpx::ParseGpx(path);
+  const auto distance = gpx.GetLength2D();
+  std::println("Distance 2D: {}", distance);
+
+  return 0;
+}
