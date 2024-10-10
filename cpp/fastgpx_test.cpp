33388052fca8be740f50349be7ab201f102528fa
Thomas Thomassen <thomas@thomthom.net>
2024-10-10 20:20:41 +0200

    Simple test and real world GPX file test.

--- cpp/fastgpx_test.cpp
+++ cpp/fastgpx_test.cpp
@@ -1,35 +1,65 @@
 #include "fastgpx.hpp"
 
+#include <filesystem>
+
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
-
-#include <filesystem>
 
 using Catch::Matchers::WithinRel;
 
 TEST_CASE("GPX files are parsed", "[fastgpx]")
 {
-  // TODO: Find a portable way to resolve the path to the test files.
-  const auto path = std::filesystem::path("../../../gpx/test/debug-segment.gpx");
-  const auto gpx = fastgpx::ParseGpx(path);
-
-  SECTION("loads all the tracks and segments")
+  SECTION("two-point single segment track")
   {
-    REQUIRE(gpx.tracks.size() == 1);
-    REQUIRE(gpx.tracks[0].segments.size() == 1);
+    // TODO: Find a portable way to resolve the path to the test files.
+    const auto path = std::filesystem::path("../../../gpx/test/debug-segment.gpx");
+    const auto gpx = fastgpx::ParseGpx(path);
+
+    SECTION("loads all the tracks and segments")
+    {
+      REQUIRE(gpx.tracks.size() == 1);
+      REQUIRE(gpx.tracks[0].segments.size() == 1);
+    }
+
+    SECTION("computes 2D distances")
+    {
+      CHECK_THAT(gpx.tracks[0].segments[0].GetLength2D(), WithinRel(1.38515855107115149));
+      CHECK_THAT(gpx.tracks[0].GetLength2D(), WithinRel(1.38515855107115149));
+      CHECK_THAT(gpx.GetLength2D(), WithinRel(1.38515855107115149));
+    }
+
+    SECTION("computes 3D distances")
+    {
+      CHECK_THAT(gpx.tracks[0].segments[0].GetLength3D(), WithinRel(1.70840984883766467));
+      CHECK_THAT(gpx.tracks[0].GetLength3D(), WithinRel(1.70840984883766467));
+      CHECK_THAT(gpx.GetLength3D(), WithinRel(1.70840984883766467));
+    }
   }
 
-  SECTION("computes 2D distances")
+  SECTION("real world GPX file")
   {
-    REQUIRE_THAT(gpx.tracks[0].segments[0].GetLength2D(), WithinRel(1.38515855107115149));
-    REQUIRE_THAT(gpx.tracks[0].GetLength2D(), WithinRel(1.38515855107115149));
-    REQUIRE_THAT(gpx.GetLength2D(), WithinRel(1.38515855107115149));
-  }
+    // TODO: Find a portable way to resolve the path to the test files.
+    const auto path = std::filesystem::path("../../../gpx/2024 TopCamp/Connected_20240518_094959_.gpx");
+    const auto gpx = fastgpx::ParseGpx(path);
 
-  SECTION("computes 3D distances")
-  {
-    REQUIRE_THAT(gpx.tracks[0].segments[0].GetLength3D(), WithinRel(1.70840984883766467));
-    REQUIRE_THAT(gpx.tracks[0].GetLength3D(), WithinRel(1.70840984883766467));
-    REQUIRE_THAT(gpx.GetLength3D(), WithinRel(1.70840984883766467));
+    SECTION("loads all the tracks and segments")
+    {
+      REQUIRE(gpx.tracks.size() == 1);
+      REQUIRE(gpx.tracks[0].segments.size() == 9);
+    }
+
+    SECTION("computes 2D distances")
+    {
+      CHECK_THAT(gpx.tracks[0].segments[0].GetLength2D(), WithinRel(17824.19175882741183159));
+      CHECK_THAT(gpx.tracks[0].GetLength2D(), WithinRel(383273.40397503407439217));
+      CHECK_THAT(gpx.GetLength2D(), WithinRel(383273.40397503407439217));
+    }
+
+    SECTION("computes 3D distances")
+    {
+      CHECK_THAT(gpx.tracks[0].segments[0].GetLength3D(), WithinRel(17859.48257500379986595));
+      CHECK_THAT(gpx.tracks[0].GetLength3D(), WithinRel(383685.94684573041740805));
+      CHECK_THAT(gpx.GetLength3D(), WithinRel(383685.94684573041740805));
+    }
   }
 }
