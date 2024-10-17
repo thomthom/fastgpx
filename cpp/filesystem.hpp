55e8da7728c36e81894da987ca2bf677564d38bd
Thomas Thomassen <thomas@thomthom.net>
2024-10-17 18:08:48 +0200

    Extract the utf8 filepath utilities to a shared file.

---
+++ cpp/filesystem.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <filesystem>
+#include <string>
+
+namespace fastgpx
+{
+
+  std::wstring utf8_to_utf16(const std::string &utf8_str);
+
+  /**
+   * @brief Opens a file for reading in binary mode given a UTF-8 path.
+   *
+   * @param file_path UTF-8 file path.
+   * @return FILE*
+   */
+  FILE *open_file(const std::filesystem::path &file_path);
+
+} // namespace fastgpx
