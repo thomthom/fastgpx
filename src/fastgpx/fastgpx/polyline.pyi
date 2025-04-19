6a893dac5a759de1fbaa335365a99174d408db01
Thomas Thomassen <thomas@thomthom.net>
2025-04-19 23:52:04 +0200

    Update pybind11 generated stubs.

--- src/fastgpx/fastgpx/polyline.pyi
+++ src/fastgpx/fastgpx/polyline.pyi
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import fastgpx
+import fastgpx.fastgpx
 import typing
 __all__ = ['Precision', 'decode', 'encode']
 class Precision:
@@ -13,9 +14,6 @@ class Precision:
     Five: typing.ClassVar[Precision]  # value = <Precision.Five: 5>
     Six: typing.ClassVar[Precision]  # value = <Precision.Six: 6>
     __members__: typing.ClassVar[dict[str, Precision]]  # value = {'Five': <Precision.Five: 5>, 'Six': <Precision.Six: 6>}
-    @staticmethod
-    def _pybind11_conduit_v1_(*args, **kwargs):
-        ...
     def __eq__(self, other: typing.Any) -> bool:
         ...
     def __getstate__(self) -> int:
@@ -43,14 +41,14 @@ class Precision:
     def value(self) -> int:
         ...
 @typing.overload
-def decode(encoded: str, precision: Precision = Precision.Five) -> list[fastgpx.LatLong]:
+def decode(encoded: str, precision: Precision = fastgpx.polyline.Precision.Five) -> list[fastgpx.fastgpx.LatLong]:
     ...
 @typing.overload
-def decode(locations: str, precision: int = 5) -> list[fastgpx.LatLong]:
+def decode(locations: str, precision: int = 5) -> list[fastgpx.fastgpx.LatLong]:
     ...
 @typing.overload
-def encode(locations: list[fastgpx.LatLong], precision: Precision = Precision.Five) -> str:
+def encode(locations: list[fastgpx.fastgpx.LatLong], precision: Precision = fastgpx.polyline.Precision.Five) -> str:
     ...
 @typing.overload
-def encode(locations: list[fastgpx.LatLong], precision: int = 5) -> str:
+def encode(locations: list[fastgpx.fastgpx.LatLong], precision: int = 5) -> str:
     ...
