ecf145baffe990c88725ed061e6514e49fd8e37a
Thomas Thomassen <thomas@thomthom.net>
2025-12-02 12:58:05 +0100

    Move the generated stubs to the root of the library file structure.

--- src/fastgpx/__init__.py
+++ src/fastgpx/__init__.py
@@ -1,5 +1,12 @@
 # Re-export the C extension module to make it available as fastgpx instead of fastgpx.fastgpx.
-from .fastgpx import *  # type: ignore
+from .fastgpx import Bounds, Gpx, LatLong, Segment, TimeBounds, Track, parse  # type: ignore
+from .fastgpx import geo, polyline  # type: ignore
+
+# Ensure submodules are importable as fastgpx.geo and fastgpx.polyline.
+import sys
+sys.modules['fastgpx.geo'] = geo
+sys.modules['fastgpx.polyline'] = polyline
 
 # Needed for sphinx autosummary to pick up the re-exported C extension module.
-__all__ = [name for name in dir(fastgpx) if not name.startswith('_')]  # type: ignore
+__all__: list = ['Bounds', 'Gpx', 'LatLong', 'Segment',
+                 'TimeBounds', 'Track', 'parse']
